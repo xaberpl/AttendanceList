5ce4f56ae8ebe950f445c3a7a281e6d4ef853190
xaberpl <rav.waszak@gmail.com>
2020-12-15 15:55:38 +0100

    create login GUI

---
+++ AttendanceList/LoginWindow.Designer.cs
@@ -0,0 +1,110 @@
+﻿
+namespace AttendanceList
+{
+    partial class LoginWindow
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tLogin = new System.Windows.Forms.TextBox();
+            this.tPassword = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(71, 58);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(53, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Password";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(71, 32);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(33, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Login";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // tLogin
+            // 
+            this.tLogin.Location = new System.Drawing.Point(148, 29);
+            this.tLogin.Name = "tLogin";
+            this.tLogin.Size = new System.Drawing.Size(100, 20);
+            this.tLogin.TabIndex = 2;
+            // 
+            // tPassword
+            // 
+            this.tPassword.Location = new System.Drawing.Point(148, 55);
+            this.tPassword.Name = "tPassword";
+            this.tPassword.Size = new System.Drawing.Size(100, 20);
+            this.tPassword.TabIndex = 3;
+            // 
+            // button1
+            // 
+            this.button1.DialogResult = System.Windows.Forms.DialogResult.OK;
+            this.button1.Location = new System.Drawing.Point(74, 91);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(174, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "LOGIN";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // LoginWindow
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(335, 153);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.tPassword);
+            this.Controls.Add(this.tLogin);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "LoginWindow";
+            this.Text = "Login Panel";
+            this.Load += new System.EventHandler(this.LoginWindow_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox tLogin;
+        private System.Windows.Forms.TextBox tPassword;
+        private System.Windows.Forms.Button button1;
+    }
+}
